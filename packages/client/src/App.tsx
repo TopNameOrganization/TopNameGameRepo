cfd7c42baeed39bcdecdb5a58338d31ee048dd7b
octo <v.minaev@qiwi.com>
2023-02-04 17:07:42 +0300

    add leaderboard page no api

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,14 @@
-import { useEffect } from 'react'
-import './App.css'
+import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material'
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  const theme = createTheme()
 
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  return (
+    <ThemeProvider theme={theme}>
+      Вот тут будет жить ваше приложение :)
+    </ThemeProvider>
+  )
 }
 
 export default App
