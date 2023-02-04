4550e709e1342addad4815dd676411627fe58b63
octo <v.minaev@qiwi.com>
2023-02-04 11:01:39 +0300

    the bigining

---
+++ packages/client/src/pages/LeaderBoardPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeaderBoardPage = () => {
+    // это должно быть из стора, а там из апи, но это неточно ))
+    const data = {}
+  return <div></div>
+}
+
+export default LeaderBoardPage;
