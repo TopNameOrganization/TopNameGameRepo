2a242a2bd1f9e0c31a49fdd3e80b3ad53eb67a74
AlexWeb79 <alexwev1979@gmail.com>
2023-02-04 19:17:17 +0100

    remove ThemeProvider from profile page

--- packages/client/src/pages/profile/Profile.tsx
+++ packages/client/src/pages/profile/Profile.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Profile from "../../components/profile/Profile";
 
-const theme = createTheme();
-
 export default function ProfilePage() {
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <Typography component="h1" variant="h5">
-                    Profile page
-                </Typography>
-                <Profile />
-            </Container>
-        </ThemeProvider>
+        <Container component="main" maxWidth="xs">
+            <Typography component="h1" variant="h5">
+                Profile page
+            </Typography>
+            <Profile />
+        </Container>
     );
 }
