9b088e9afa905361c0d2b683cab4db0c0f85095f
AlexWeb79 <alexwev1979@gmail.com>
2023-02-06 17:27:06 +0100

    moved const validationShema outside the component and fix state fields form

--- packages/client/src/components/profile/ProfileForm.tsx
+++ packages/client/src/components/profile/ProfileForm.tsx
@@ -9,59 +9,50 @@ interface IProfile {
     [key: string]: FormDataEntryValue | null
 }
 
+const validationSchema = yup.object({
+    firstName: yup
+        .string()
+        .min(3, 'First name should be of minimum 3 characters length')
+        .max(20, 'First name should be of maximum 20 characters length'),
+    secondName: yup
+        .string()
+        .min(3, 'Second name should be of minimum 3 characters length')
+        .max(20, 'First name should be of maximum 20 characters length'),
+    nickName: yup
+        .string()
+        .min(3, 'Password should be of minimum 3 characters length')
+        .max(20, 'First name should be of maximum 20 characters length')
+        .required('Nick name is required'),
+    email: yup
+        .string()
+        .email('Enter a valid email')
+        .required('Email is required'),
+    phone: yup
+        .string()
+        .min(12, 'Phone should be of minimum 12 characters length')
+        .max(12, 'Phone should be of maximum 12 characters length')
+        .required('Phone is required'),
+    password: yup
+        .string()
+        .min(8, 'Password should be of minimum 8 characters length')
+        .required('Password is required'),
+});
+
 export default function ProfileForm() {
     const [isDisabled, setIsDisabled] = React.useState<boolean>(false);
-    const [profile, setProfile] = React.useState<IProfile>({
-        firstName: '',
-        secondName: '',
-        nickName: '',
-        email: '',
-        phone: '',
-        password: ''
-    });
-
-    const validationSchema = yup.object({
-        firstName: yup
-            .string()
-            .min(3, 'First name should be of minimum 3 characters length')
-            .max(20, 'First name should be of maximum 20 characters length'),
-        secondName: yup
-            .string()
-            .min(3, 'Second name should be of minimum 3 characters length')
-            .max(20, 'First name should be of maximum 20 characters length'),
-        nickName: yup
-            .string()
-            .min(3, 'Password should be of minimum 3 characters length')
-            .max(20, 'First name should be of maximum 20 characters length')
-            .required('Nick name is required'),
-        email: yup
-            .string()
-            .email('Enter a valid email')
-            .required('Email is required'),
-        phone: yup
-            .string()
-            .min(12, 'Phone should be of minimum 12 characters length')
-            .max(12, 'Phone should be of maximum 12 characters length')
-            .required('Phone is required'),
-        password: yup
-            .string()
-            .min(8, 'Password should be of minimum 8 characters length')
-            .required('Password is required'),
-    });
 
     const formik = useFormik({
         initialValues: {
-            firstName: profile.firstName,
-            secondName: profile.secondName,
-            nickName: profile.nickName,
-            phone: profile.phone,
-            email: profile.email,
-            password: profile.password,
+            firstName: '',
+            secondName: '',
+            nickName: '',
+            phone: '',
+            email: '',
+            password: '',
         },
         validationSchema: validationSchema,
         onSubmit: (profileData: IProfile) => {
             console.log('profileData: ', profileData)
-            setProfile(profileData);
             setIsDisabled(false);
         },
     });
