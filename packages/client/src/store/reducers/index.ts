95afb5ab7b1604eb484c9fb5d85efd6e49fcfb2f
Aleksandr <alexweb1979@gmail.com>
2023-02-17 19:51:06 +0100

    fix upload avatar

--- packages/client/src/store/reducers/index.ts
+++ packages/client/src/store/reducers/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReduser";
 import { changeUserProfileReducer } from "./changeUserProfileReducer";
-import { changeUserAvatarReducer } from "./changeUserAvatarReducer";
 
 export const rootReducer = combineReducers({
   user: userReducer,
-  changeUserProfile: changeUserProfileReducer,
-  changeUserAvatar: changeUserAvatarReducer
+  changeUserProfile: changeUserProfileReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
