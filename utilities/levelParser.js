d3852fe4867fc22f45f0db33a19c01c765ae4b6d
octo <v.minaev@qiwi.com>
2023-02-22 23:31:45 +0300

    parse image

---
+++ utilities/levelParser.js
@@ -0,0 +1,88 @@
+var fs = require('fs')
+var { createCanvas, loadImage } = require('canvas')
+
+const tilesMap = [
+  'blackblack',
+  'redblack',
+  'redred',
+  'whitewhite',
+  'whiteblack',
+  'blackyellow',
+  'purpleblack',
+  'greyblack',
+]
+
+const tileSize = 8
+const width = 32
+const height = 22
+const offsetX = 19
+const offsetY = 19
+const levelStepX = 277
+const levelStepY = 213
+
+check = [
+  { x: 1, y: 0 },
+  { x: 6, y: 3 },
+]
+
+loadImage(__dirname + '/levels.png').then(img => {
+  const getColorName = data => {
+    if (data[0] === 0 && data[1] === 0 && data[2] === 0) {
+      return 'black'
+    }
+    if (data[0] === 255 && data[1] === 0 && data[2] === 0) {
+      return 'red'
+    }
+    if (data[0] === 255 && data[1] === 255 && data[2] === 255) {
+      return 'white'
+    }
+    if (data[0] === 255 && data[1] === 255 && data[2] === 0) {
+      return 'yellow'
+    }
+    if (data[0] === 255 && data[1] === 0 && data[2] === 255) {
+      return 'purple'
+    }
+    if (data[0] === 204 && data[1] === 204 && data[2] === 204) {
+      return 'grey'
+    }
+  }
+
+  const getTileAt = (x, y) => {
+    const offset = { x: x * tileSize, y: y * tileSize }
+    const color1 = getColorName(
+      ctx.getImageData(offset.x + check[0].x, offset.y + check[0].y, 1, 1).data
+    )
+    const color2 = getColorName(
+      ctx.getImageData(offset.x + check[1].x, offset.y + check[1].y, 1, 1).data
+    )
+    return tilesMap.indexOf(`${color1}${color2}`)
+  }
+
+  const canvasWidth = width * tileSize
+  const canvasHeight = height * tileSize
+  const canvas = createCanvas(canvasWidth, canvasHeight)
+  const ctx = canvas.getContext('2d')
+
+  ctx.drawImage(
+    img,
+    offsetX,
+    offsetY,
+    canvasWidth,
+    canvasHeight,
+    0,
+    0,
+    canvasWidth,
+    canvasHeight
+  )
+
+  const levelMap = []
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      if (!levelMap[y]) {
+        levelMap[y] = []
+      }
+      levelMap[y].push(getTileAt(x, y))
+    }
+  }
+  console.log(levelMap)
+})
