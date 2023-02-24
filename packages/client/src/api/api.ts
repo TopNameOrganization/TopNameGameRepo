d15281337f50e62429b1bae5afd47810c8f94e87
octo <v.minaev@qiwi.com>
2023-02-25 01:32:24 +0300

    game api the bigining

--- packages/client/src/api/api.ts
+++ packages/client/src/api/api.ts
@@ -3,6 +3,7 @@ import axios, { AxiosInstance } from 'axios'
 const baseURL = 'https://ya-praktikum.tech/api/v2'
 
 let instance: AxiosInstance
+let instanceLocal: AxiosInstance
 
 const getInstance = () => {
   if (!instance) {
@@ -22,4 +23,18 @@ const getInstance = () => {
   return instance
 }
 
+const getInstanceLocal = () => {
+  if (!instanceLocal) {
+    instanceLocal = axios.create({
+      baseURL: '',
+      timeout: 1000,
+      withCredentials: true,
+      responseType: 'blob',
+    })
+  }
+
+  return instanceLocal
+}
+
 export const api = getInstance()
+export const apiFile = getInstanceLocal()
