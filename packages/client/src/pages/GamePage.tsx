e7d6629ef623dab6d7aea5c9f48113d38b7ae021
octo <v.minaev@qiwi.com>
2023-02-25 18:48:29 +0300

    some fixes

--- packages/client/src/pages/GamePage.tsx
+++ packages/client/src/pages/GamePage.tsx
@@ -1,18 +1,7 @@
 import React, { useLayoutEffect } from 'react'
 
-import { Level } from '../components/game/constants'
-import { Level2 } from '../components/game/constants'
-
 import { GameView } from '../components/game/view/GameView'
-import GameModel from '../components/game/model/GameModel'
-import { ModelEvents } from '../components/game/model'
 
 export const GamePage = () => {
-  // GameModel.setLevel({ level: Level })
-  // GameModel.on(ModelEvents.LevelUp, () => {
-  //   GameModel.setLevel({ level: Level2 })
-  //   GameModel.dispatchUpdate()
-  // })
-
   return <GameView />
 }
