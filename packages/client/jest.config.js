bc664c626e86d53e8079914d63a8476f779e62b3
prchnk <job-eng@yandex.ru>
2023-02-25 21:13:16 +0300

    fix test

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -8,4 +8,8 @@ export default {
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  "moduleNameMapper": {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/img-mock.js",
+    "\\.(css)$": "<rootDir>/img-mock.js",
+  }
 }
