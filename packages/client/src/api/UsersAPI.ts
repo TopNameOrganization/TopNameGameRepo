d04205da2be48f07e7c4ca10e5b750077c1c6883
octo <v.minaev@qiwi.com>
2023-02-28 13:54:01 +0300

    review fix more better

--- packages/client/src/api/UsersAPI.ts
+++ packages/client/src/api/UsersAPI.ts
@@ -5,15 +5,15 @@ import {AxiosResponse} from 'axios';
 class UsersAPI {
 
   static changeUserProfile = async (data: UserProfileData) => {
-    return await instance.put<string, AxiosResponse>(`/user/profile`, data);
+    return await instance.put<string, AxiosResponse<User>>(`/user/profile`, data);
   }
 
   static changeUserAvatar = async (data: FormData) => {
-    return await instance.put<string, AxiosResponse>(`/user/profile/avatar`, data);
+    return await instance.put<string, AxiosResponse<User>>(`/user/profile/avatar`, data);
   }
 
   static changeUserPassword = async (data: UserPasswordData) => {
-    return await instance.put<string, AxiosResponse>(`/user/password`, data);
+    return await instance.put<string, AxiosResponse<string>>(`/user/password`, data);
   }
 
   static getUserById = async (id: number) => {
